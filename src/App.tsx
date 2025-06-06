3c2cc2ab92ec70de79fc2bc6884efd2b56caebe5
Yongchan Kim <dydydade@gmail.com>
2025-06-06 17:44:51 +0900

    [feat] add interface, default values, and optional field ~ with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,11 @@
-import styled from "styled-components";
-import { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-  background-color: ${props => props.theme.backgroundColor};
-  width: 100vw;
-  height: 100vh;
-  display:flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const animation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  } 
-  50% {
-    /* transform: rotate(360deg); */
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-    font-size: 28px;
-    cursor: pointer;
-`
-
-const Box = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: ${props => props.theme.textColor};
-  animation: ${animation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji}:hover {
-    font-size: 42px;
-  }
-`;
-
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <Emoji as="p">😊</Emoji>
-      </Box>
-      <Emoji as="p">🚀</Emoji>
-    </Wrapper>
+    <div>
+      <Circle borderColor="yellow" bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
---
+++ src/Circle.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+    bgColor: string;
+    borderColor: string;
+}
+
+interface CircleProps {
+    bgColor: string;
+    borderColor?: string;
+    text?: string;
+}
+
+const Container = styled.div<ContainerProps>`
+    width: 200px;
+    height: 200px;
+    background-color: ${(props) => props.bgColor};
+    border-radius: 50%;
+    border: 1px solid ${(props) => props.borderColor};
+`;
+
+function Circle({ bgColor, borderColor, text = "default value" }: CircleProps) {
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>{text}</Container>
+}
+
+export default Circle;
