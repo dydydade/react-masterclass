a8ace80252d41fc7aa4e491306730b46b6127878
Yongchan Kim <dydydade@gmail.com>
2025-06-06 12:40:31 +0900

    [feat] add my first styled componenets

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display:flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  background-color: ${(props) => props.bgColor};
+  width:100px;
+  heigth: 100px;
+  border-radius: 50px;
+`
+
+function App() {
+  return (
+    <Father>
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </Father>
+  );
+}
+
+export default App;
